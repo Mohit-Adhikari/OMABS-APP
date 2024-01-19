8ae3bf05daef3d645a11d0e13cda11a26019f254
Mohit Adhikari <adhikari.mhtp@gmail.com>
2024-01-19 22:22:45 +0545

    Calculated Age

--- app/src/main/java/com/example/a4thsemproject/customerInfo.java
+++ app/src/main/java/com/example/a4thsemproject/customerInfo.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,6 +20,7 @@ public class customerInfo extends AppCompatActivity {
         setContentView(R.layout.customer_info);
         name=findViewById(R.id.editTextName);
          dob=findViewById(R.id.editTextDateOfBirth);
+
         name.getText(); //get text from the user
         dob.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,8 +42,26 @@ public class customerInfo extends AppCompatActivity {
             public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDayOfMonth) {
                 String selectedDate = selectedDayOfMonth + "/" + (selectedMonth + 1) + "/" + selectedYear;
                 dob.setText(selectedDate);
+                calculateAge(selectedYear, selectedMonth, selectedDayOfMonth);
             }
         },year,month,dayOfMonth);
         datePickerDialog.show();
+
+    }
+    private void calculateAge(int year, int month, int day) {
+        Calendar dobCalendar = Calendar.getInstance();
+        dobCalendar.set(year, month, day);
+
+        Calendar currentCalendar = Calendar.getInstance();
+
+        int age = currentCalendar.get(Calendar.YEAR) - dobCalendar.get(Calendar.YEAR);
+
+        if (currentCalendar.get(Calendar.DAY_OF_YEAR) < dobCalendar.get(Calendar.DAY_OF_YEAR)) {
+            age--;
+        }
+
+//        String ageTextString = getString(R.string.age_text, age);
+//        ageText.setText(ageTextString);
+        Log.i("Age", String.valueOf(age));
     }
 }
