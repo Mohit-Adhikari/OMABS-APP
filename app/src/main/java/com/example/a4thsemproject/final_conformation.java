d875f8b83b4904ca39f0bb9cf6189336a7a67130
Mohit Adhikari <adhikari.mhtp@gmail.com>
2024-01-25 21:08:15 +0545

    Added patient info to the realtime

--- app/src/main/java/com/example/a4thsemproject/final_conformation.java
+++ app/src/main/java/com/example/a4thsemproject/final_conformation.java
@@ -1,6 +1,7 @@
 package com.example.a4thsemproject;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -9,6 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class final_conformation extends AppCompatActivity {
     String name;
@@ -33,7 +37,18 @@ public class final_conformation extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 databaseReference.child(time).setValue(false);
+                String name_p=DataHolder.getInstance().getName();
+                String age_p=DataHolder.getInstance().getAge();
+                String gender_p=DataHolder.getInstance().getGender();
+                Map<String,Object> patient_Data=new HashMap<>();
+                //patient_Data.put("name",name_p);
+                patient_Data.put("age",age_p);
+                patient_Data.put("gender",gender_p);
+                Log.i("patient",name_p);
+                databaseReference.child(time).child(name_p).setValue(patient_Data);
+
             }
         });
 
